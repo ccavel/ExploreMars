1cb9a915ec301d1621199d9edc06980302290779
mahautm <45102506+mahautm@users.noreply.github.com>
2019-05-09 18:37:11 +0200

    modification data et calcule similarite

--- analyse similarite questionnaire/Scripte/Preparation Variable.R
+++ analyse similarite questionnaire/Scripte/Preparation Variable.R
@@ -4,6 +4,10 @@ rm(list = ls())
 #Import des données
 data <- read.table("Data/data.txt", header=TRUE)
 
+#Partie entiere de MoyEcartCroix
+#table = as.integer(data[,"MoyEcartCroix"])
+#trunc(data[,"MoyEcartCroix"])
+
 data.reussite = data[data[,"TrouveRoche"]=="OUI",]
 data.echec = data[data[,"TrouveRoche"]=="NON",]
 

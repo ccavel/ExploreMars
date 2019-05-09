fb561110fcb7232882991da927c68bbe2a142829
mahautm <45102506+mahautm@users.noreply.github.com>
2019-05-09 17:34:44 +0200

    update calcule similarite

--- analyse similarite questionnaire/Scripte/Calcule similarite.R
+++ analyse similarite questionnaire/Scripte/Calcule similarite.R
@@ -7,30 +7,24 @@ vectSimilarite.echec = vector("numeric",length(data.echec[,1]))
 similarite = 0
 for (i in seq(length(data.reussite[,1])) ){
   similarite = 0
-  for (variable in c("HabitudeTravaillerEnsemble","DifficulteTrouverRoche","BonneCommunication","PerformancePerso","PerformanceCoeq","EquilibreParole","EquilibreQuestions","BonneEquipe","GeneVisionCoeq","PdCs") ){
+  for (variable in c("PdCs") ){
+  #for (variable in c("HabitudeTravaillerEnsemble","DifficulteTrouverRoche","BonneCommunication","EquilibreParole","EquilibreQuestions","PdCs") ){
     similarite = similarite + data.reussite[i,variable]
   }
   vectSimilarite.reussite[i] = similarite
 }
 rm(similarite,i,variable)
 
-# Remplissage du vecteur des similarites pour la echec
-similarite = 0
-for (i in seq(length(data.echec[,1])) ){
-  similarite = 0
-  for (variable in c("HabitudeTravaillerEnsemble","DifficulteTrouverRoche","BonneCommunication","PerformancePerso","PerformanceCoeq","EquilibreParole","EquilibreQuestions","BonneEquipe","GeneVisionCoeq","PdCs") ){
-    similarite = similarite + data.echec[i,variable]
-  }
-  vectSimilarite.echec[i] = similarite
-}
-rm(similarite,i,variable)
-
-
-par(mfrow=c(1,2))
 
 plot(data.reussite[,"ChronoSec"], vectSimilarite.reussite, main="Réussite",
      xlab="performace temporel", ylab="similarité des réponses")
 
-plot(data.echec[,"ChronoSec"], vectSimilarite.echec, main="echec",
-     xlab="performace temporel", ylab="similarité des réponses")
 
+#table = data.reussite[data.reussite[,"PdCs"]==1,]
+
+
+boxplot(data.reussite[data.reussite[,"PdCs"]==1,][,"ChronoSec"],data.reussite[data.reussite[,"PdCs"]==0,][,"ChronoSec"] , main="PdCs")
+
+boxplot(data.reussite[data.reussite[,"EquilibreParole"]==1,][,"ChronoSec"],data.reussite[data.reussite[,"EquilibreParole"]==0,][,"ChronoSec"] , main="EquilibreParole", xlabel=c("1","0"))
+
+boxplot(data.reussite[data.reussite[,"EquilibreQuestions"]==1,][,"ChronoSec"],data.reussite[data.reussite[,"EquilibreQuestions"]==0,][,"ChronoSec"] , main="EquilibreQuestions")
